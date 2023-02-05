31c790adf4c50d84a145538a7164f98685192096
filian <filiandun@gmail.com>
2023-02-05 14:14:29 +0300

    project: 100% ready

--- class Base/base.h
+++ class Base/base.h
@@ -1,7 +1,93 @@
 #pragma once
+#include <iostream>
 
+
+template<typename T>
 class Base
 {
+public:
+	Base() = default;
 
+	Base(T value_1, T value_2)
+	{
+		this->value_1 = value_1;
+		this->value_2 = value_2;
+	}
+
+	~Base() = default;
+
+protected:
+	T value_1;
+	T value_2;
+
+public:
+	void display_base() const
+	{
+		std::cout << "Значение 1: " << this->value_1 << std::endl
+			<< "Значение 2: " << this->value_2 << std::endl << std::endl;
+	}
 };
 
+
+
+
+template<typename T>
+class Sqrt : public Base<T>
+{
+public:
+	Sqrt() = delete;
+
+	Sqrt(T value_1, T value_2)
+	{
+		this->value_1 = value_1;
+		this->value_2 = value_2;
+
+		this->sqrt_value_1 = sqrt(this->value_1);
+		this->sqrt_value_2 = sqrt(this->value_2);
+	}
+
+	~Sqrt() = default;
+
+private:
+	T sqrt_value_1;
+	T sqrt_value_2;
+
+public:
+	void display_sqrt() const
+	{
+		std::cout << "Значение 1 в корне: " << this->sqrt_value_1 << std::endl
+			<< "Значение 2 в корне: " << this->sqrt_value_2 << std::endl << std::endl;
+	}
+};
+
+
+
+
+template<typename T>
+class Pow : public Base<T>
+{
+public:
+	Pow() = delete;
+
+	Pow(T value_1, T value_2)
+	{
+		this->value_1 = value_1;
+		this->value_2 = value_2;
+
+		this->pow_value_1 = (T) pow(this->value_1, 2);
+		this->pow_value_2 = (T) pow(this->value_2, 2);
+	}
+
+	~Pow() = default;
+
+private:
+	T pow_value_1;
+	T pow_value_2;
+
+public:
+	void display_pow() const
+	{
+		std::cout << "Значение 1 в квадрате: " << this->pow_value_1 << std::endl
+			<< "Значение 2 в квадрате: " << this->pow_value_2 << std::endl << std::endl;
+	}
+};
--- class Base/main.cpp
+++ class Base/main.cpp
@@ -1,8 +1,37 @@
 ﻿#include <iostream>
+#include "base.h"
+
+
 
 int main()
 {
     setlocale(LC_ALL, "rus");
 
 
+// БАЗОВЫЙ КЛАСС
+    std::cout << "БАЗОВЫЙ КЛАСС: " << std::endl;
+
+    Base<int> base(260, 23);
+    base.display_base();
+
+
+// КЛАСС ИЗВЛЕЧЕНИЯ КВАДРАТНОГО КОРНЯ
+    std::cout << "КЛАСС ИЗВЛЕЧЕНИЯ КВАДРАТНОГО КОРНЯ: " << std::endl;
+
+    Sqrt<int> sqrt(121, 64);
+    sqrt.display_base();
+    sqrt.display_sqrt();
+
+
+// КЛАСС ВОЗВЕДЕНИЯ В КВАДРАТ
+    std::cout << "КЛАСС ВОЗВЕДЕНИЯ В КВАДРАТ: " << std::endl;
+
+    Pow<int> pow(4, 16);
+    pow.display_base();
+    pow.display_pow();
+
+
+
+
+    return 0;
 }
