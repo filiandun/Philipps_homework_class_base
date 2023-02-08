ed08d2dd925324e9af6df25ca36ea3892523ef99
filian <filiandun@gmail.com>
2023-02-08 18:42:14 +0300

    project: 101% ready

--- class Base/base.h
+++ class Base/base.h
@@ -8,11 +8,18 @@ class Base
 public:
 	Base() = default;
 
+	Base(const Base& base)
+	{
+		this->value_1 = base.value_1;
+		this->value_2 = base.value_2;
+	}
+
 	Base(T value_1, T value_2)
 	{
 		this->value_1 = value_1;
 		this->value_2 = value_2;
 	}
+
 
 	~Base() = default;
 
@@ -37,13 +44,22 @@ class Sqrt : public Base<T>
 public:
 	Sqrt() = delete;
 
+	Sqrt(const Sqrt& sqrt)
+	{
+		this->value_1 = sqrt.value_1;
+		this->value_2 = sqrt.value_2;
+
+		this->sqrt_value_1 = sqrt.sqrt_value_1;
+		this->sqrt_value_2 = sqrt.sqrt_value_2;
+	}
+
 	Sqrt(T value_1, T value_2)
 	{
 		this->value_1 = value_1;
 		this->value_2 = value_2;
 
-		this->sqrt_value_1 = sqrt(this->value_1);
-		this->sqrt_value_2 = sqrt(this->value_2);
+		this->sqrt_value_1 = (T) sqrt(this->value_1);
+		this->sqrt_value_2 = (T) sqrt(this->value_2);
 	}
 
 	~Sqrt() = default;
@@ -68,6 +84,15 @@ class Pow : public Base<T>
 {
 public:
 	Pow() = delete;
+
+	Pow(const Pow& pow)
+	{
+		this->value_1 = pow.value_1;
+		this->value_2 = pow.value_2;
+
+		this->pow_value_1 = pow.pow_value_1;
+		this->pow_value_2 = pow.pow_value_2;
+	}
 
 	Pow(T value_1, T value_2)
 	{
--- class Base/main.cpp
+++ class Base/main.cpp
@@ -14,6 +14,13 @@ int main()
     Base<int> base(260, 23);
     base.display_base();
 
+    // КОНСТРУКТОР КОПИРОВАНИЯ КЛАССА BASE
+    std::cout << "Конструктор копирования класса base: " << std::endl;
+
+    Base<int> base_copy = base;
+    base_copy.display_base();
+
+
 
 // КЛАСС ИЗВЛЕЧЕНИЯ КВАДРАТНОГО КОРНЯ
     std::cout << "КЛАСС ИЗВЛЕЧЕНИЯ КВАДРАТНОГО КОРНЯ: " << std::endl;
@@ -22,6 +29,14 @@ int main()
     sqrt.display_base();
     sqrt.display_sqrt();
 
+    // КОНСТРУКТОР КОПИРОВАНИЯ КЛАССА SQRT
+    std::cout << "Конструктор копирования класса sqrt: " << std::endl;
+
+    Sqrt<int> sqrt_copy = sqrt;
+    sqrt_copy.display_base();
+    sqrt_copy.display_sqrt();
+
+
 
 // КЛАСС ВОЗВЕДЕНИЯ В КВАДРАТ
     std::cout << "КЛАСС ВОЗВЕДЕНИЯ В КВАДРАТ: " << std::endl;
@@ -29,6 +44,14 @@ int main()
     Pow<int> pow(4, 16);
     pow.display_base();
     pow.display_pow();
+
+    // КОНСТРУКТОР КОПИРОВАНИЯ КЛАССА POW
+    std::cout << "Конструктор копирования класса pow: " << std::endl;
+
+    Pow<int> pow_copy = pow;
+    pow_copy.display_base();
+    pow_copy.display_pow();
+
 
 
 
